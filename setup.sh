5d14036c27e817a8b31d2ae4bebd2e7d12ba3f0d
Fahad-Anxari-1999 <fahadanxari15@gmail.com>
2024-11-02 19:51:20 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,1 @@
-#! /bin/bash
 
-# Specify the Python version
-PYTHON_VERSION=3.10
-
-# Install the specified Python version
-sudo apt-get update
-sudo apt-get install -y python${PYTHON_VERSION} python3-pip
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python${PYTHON_VERSION} 1
-sudo apt-get install -y python3-venv
